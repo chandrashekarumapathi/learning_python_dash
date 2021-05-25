ec8b008da81e151ce41d0428a1d9581c64f96a5e
Chandrashekar <73859737+chandrashekarumapathi@users.noreply.github.com>
2021-05-25 15:59:23 +0200

    Update heatmaps_with_subplots.py

--- plotly_basics/heatmaps_with_subplots.py
+++ plotly_basics/heatmaps_with_subplots.py
@@ -3,9 +3,9 @@ import plotly.graph_objs as go
 from plotly import subplots
 import pandas as pd
 
-df1 = pd.read_csv(r'C:\Users\chand\PycharmProjects\Learning_Dash\data\2010SantaBarbaraCA.csv')
-df2 = pd.read_csv(r'C:\Users\chand\PycharmProjects\Learning_Dash\data\2010SitkaAK.csv')
-df3 = pd.read_csv(r'C:\Users\chand\PycharmProjects\Learning_Dash\data\2010YumaAZ.csv')
+df1 = pd.read_csv('SantaBarbaraCA.csv')
+df2 = pd.read_csv('SitkaAK.csv')
+df3 = pd.read_csv('YumaAZ.csv')
 
 data1 = [go.Heatmap(x=df1['DAY'], y=df1['LST_TIME'], z=df1['T_HR_AVG'].values.tolist(), colorscale='Jet', zmin=5, zmax=40)]
 
