a7339374fd9baf0de3632e0ef14d78387a089f6f
Chandrashekar <73859737+chandrashekarumapathi@users.noreply.github.com>
2021-05-25 15:58:07 +0200

    Update bubble_plots.py

--- plotly_basics/bubble_plots.py
+++ plotly_basics/bubble_plots.py
@@ -2,7 +2,7 @@ import plotly.offline as pyo
 import plotly.graph_objs as go
 import pandas as pd
 
-df = pd.read_csv(r'C:\Users\chand\PycharmProjects\Learning_Dash\data\mpg.csv')
+df = pd.read_csv('mpg.csv')
 
 data = [go.Scatter(
     x=df['horsepower'],
