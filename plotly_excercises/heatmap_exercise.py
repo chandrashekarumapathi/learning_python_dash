01b2805639c09f569cd43cd44738227987dd48f0
Chandrashekar <73859737+chandrashekarumapathi@users.noreply.github.com>
2021-05-25 16:17:13 +0200

    Update heatmap_exercise.py

--- plotly_excercises/heatmap_exercise.py
+++ plotly_excercises/heatmap_exercise.py
@@ -2,7 +2,7 @@ import plotly.offline as pyo
 import plotly.graph_objs as go
 import pandas as pd
 
-df = pd.read_csv(r'C:\Users\chand\PycharmProjects\Learning_Dash\data\flights.csv')
+df = pd.read_csv('flights.csv')
 
 data = [go.Heatmap(x=df['year'], y=df['month'], z=df['passengers'].values.tolist(), colorscale='Jet')]
 
