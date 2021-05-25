481df5152317f711d4c450288f83704d8ae4cb26
Chandrashekar <73859737+chandrashekarumapathi@users.noreply.github.com>
2021-05-25 15:57:32 +0200

    Update bar_plots.py

--- plotly_basics/bar_plots.py
+++ plotly_basics/bar_plots.py
@@ -2,7 +2,7 @@ import plotly.offline as pyo
 import plotly.graph_objs as go
 import pandas as pd
 
-df = pd.read_csv(r'C:\Users\chand\PycharmProjects\Learning_Dash\data\2018WinterOlympics.csv')
+df = pd.read_csv('WinterOlympics.csv')
 
 print(df)
 
